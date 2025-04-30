65f9dfcc0d96a065d0a255a2a1225953c051addc
FernandoVS11 <a18000997@alumnos.uady.mx>
2025-04-30 09:30:21 -0600

    fix: :bug: Fix bad functionality of exceptions

--- src/main/java/com/chiops/gateway/controllers/DriverController.java
+++ src/main/java/com/chiops/gateway/controllers/DriverController.java
@@ -2,16 +2,7 @@ package com.chiops.gateway.controllers;
 
 import com.chiops.gateway.libs.clients.DriverClient;
 import com.chiops.gateway.libs.dtos.DriverDTO;
-import com.chiops.gateway.libs.exceptions.entities.ErrorResponse;
-import com.chiops.gateway.libs.exceptions.exception.BadRequestException;
-import com.chiops.gateway.libs.exceptions.exception.InternalServerException;
-import com.chiops.gateway.libs.exceptions.exception.MethodNotAllowedException;
-import com.chiops.gateway.libs.exceptions.exception.NotFoundException;
-import io.micronaut.http.HttpRequest;
-import io.micronaut.http.HttpResponse;
-import io.micronaut.http.HttpStatus;
 import io.micronaut.http.annotation.*;
-import io.micronaut.http.annotation.Error;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
 import io.micronaut.security.annotation.Secured;
@@ -32,67 +23,26 @@ public class DriverController {
 
     @Post("/create")
     public DriverDTO createDriver(@Body DriverDTO driverDTO) {
-        try {
-            return driverClient.createDriver(driverDTO);
-        } catch (BadRequestException e) {
-            throw new BadRequestException("Bad request when creating driver: " + e.getMessage());
-        } catch (InternalServerException e) {
-            throw new InternalServerException("Internal error when creating driver: " + e.getMessage());
-        }
+        return driverClient.createDriver(driverDTO);
     }
 
     @Put("/update")
     public DriverDTO updateDriver(@Body DriverDTO driverDTO) {
-        try {
-            return driverClient.updateDriver(driverDTO);
-        } catch (BadRequestException e) {
-            throw new BadRequestException("Bad request when updating driver: " + e.getMessage());
-        } catch (InternalServerException e) {
-            throw new InternalServerException("Internal error when updating driver: " + e.getMessage());
-        }
+        return driverClient.updateDriver(driverDTO);
     }
 
     @Delete("/delete/{curp}")
     public void deleteDriver(@PathVariable String curp) {
-        try {
-            driverClient.deleteDriver(curp);
-        } catch (BadRequestException e) {
-            throw new BadRequestException("Bad request when deleting driver: " + e.getMessage());
-        } catch (InternalServerException e) {
-            throw new InternalServerException("Internal error when deleting driver: " + e.getMessage());
-        }
+        driverClient.deleteDriver(curp);
     }
 
     @Get("/get/{curp}")
     public DriverDTO getDriverByCurp(@PathVariable String curp) {
-        try {
-            return driverClient.getDriverByCurp(curp);
-        } catch (BadRequestException e) {
-            throw new BadRequestException("Bad request when fetching driver by CURP: " + e.getMessage());
-        } catch (InternalServerException e) {
-            throw new InternalServerException("Internal error when fetching driver by CURP: " + e.getMessage());
-        }
+        return driverClient.getDriverByCurp(curp);
     }
 
     @Get("/getall")
     public List<DriverDTO> getAllDrivers() {
-        try {
-            return driverClient.getAllDrivers();
-        } catch (BadRequestException e) {
-            throw new BadRequestException("Bad request when fetching driver list: " + e.getMessage());
-        } catch (InternalServerException e) {
-            throw new InternalServerException("Internal error when fetching driver list: " + e.getMessage());
-        }
+        return driverClient.getAllDrivers();
     }
-
-    @Error(status = HttpStatus.NOT_FOUND, global = true)
-    public HttpResponse<ErrorResponse> handleNotFound(HttpRequest<?> request) {
-        throw new NotFoundException("Endpoint " + request.getPath() + " not found");
-    }
-
-    @Error(status = HttpStatus.METHOD_NOT_ALLOWED, global = true)
-    public HttpResponse<ErrorResponse> handleMethodNotAllowed(HttpRequest<?> request) {
-        throw new MethodNotAllowedException("Method " + request.getMethod() + " not allowed for " + request.getPath());
-    }
-
 }
--- src/main/java/com/chiops/gateway/controllers/VehicleController.java
+++ src/main/java/com/chiops/gateway/controllers/VehicleController.java
@@ -2,19 +2,10 @@ package com.chiops.gateway.controllers;
 
 import com.chiops.gateway.libs.clients.VehicleClient;
 import com.chiops.gateway.libs.dtos.VehicleDTO;
-import com.chiops.gateway.libs.exceptions.entities.ErrorResponse;
-import com.chiops.gateway.libs.exceptions.exception.BadRequestException;
-import com.chiops.gateway.libs.exceptions.exception.InternalServerException;
-import com.chiops.gateway.libs.exceptions.exception.MethodNotAllowedException;
-import com.chiops.gateway.libs.exceptions.exception.NotFoundException;
 import com.chiops.gateway.services.VehicleImageEncodingService;
-
-import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
-import io.micronaut.http.HttpStatus;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.*;
-import io.micronaut.http.annotation.Error;
 import io.micronaut.http.multipart.CompletedFileUpload;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
@@ -26,100 +17,52 @@ import java.util.List;
 @ExecuteOn(TaskExecutors.BLOCKING)
 @Controller("/vehicle")
 public class VehicleController {
+
     private final VehicleImageEncodingService vehicleImageEncodingService;
     private final VehicleClient vehicleClient;
 
-    public VehicleController(VehicleImageEncodingService vehicleImageEncodingService, VehicleClient vehicleClient) {
-        this.vehicleClient = vehicleClient;
+    public VehicleController(VehicleImageEncodingService vehicleImageEncodingService,
+                             VehicleClient vehicleClient) {
         this.vehicleImageEncodingService = vehicleImageEncodingService;
+        this.vehicleClient = vehicleClient;
     }
 
     @Post(value = "/create", consumes = MediaType.MULTIPART_FORM_DATA)
     public VehicleDTO createVehicle(@Part("vehicle") VehicleDTO vehicle,
                                     @Part("imageFile") CompletedFileUpload imageFile) {
-        try {
-            return vehicleImageEncodingService.createVehicleWithImageEncoded(vehicle, imageFile);
-        } catch (BadRequestException e) {
-            throw new BadRequestException("Bad request when creating vehicle: " + e.getMessage());
-        } catch (InternalServerException e) {
-            throw new InternalServerException("Internal error when creating vehicle: " + e.getMessage());
-        }
+        return vehicleImageEncodingService.createVehicleWithImageEncoded(vehicle, imageFile);
     }
-    
+
     @Put(value = "/update", consumes = MediaType.APPLICATION_JSON)
     public VehicleDTO updateVehicle(@Body VehicleDTO vehicle) {
-        try {
-            return vehicleClient.updateVehicle(vehicle);
-        } catch (BadRequestException e) {
-            throw new BadRequestException("Bad request when updating vehicle: " + e.getMessage());
-        } catch (InternalServerException e) {
-            throw new InternalServerException("Internal error when updating vehicle: " + e.getMessage());
-        }
+        return vehicleClient.updateVehicle(vehicle);
     }
 
     @Delete("/delete/{vin}")
     public void deleteVehicle(@PathVariable String vin) {
-        try {
-            vehicleClient.deleteVehicle(vin);
-        } catch (BadRequestException e) {
-            throw new BadRequestException("Bad request when deleting vehicle: " + e.getMessage());
-        } catch (InternalServerException e) {
-            throw new InternalServerException("Internal error when deleting vehicle: " + e.getMessage());
-        }
+        vehicleClient.deleteVehicle(vin);
     }
 
     @Get(value = "/get/{vin}", consumes = MediaType.APPLICATION_JSON)
     public VehicleDTO getVehicleByVin(@PathVariable String vin) {
-        try {
-            return vehicleClient.getVehicleByVin(vin);
-        } catch (BadRequestException e) {
-            throw new BadRequestException("Bad request when fetching vehicle by VIN: " + e.getMessage());
-        } catch (InternalServerException e) {
-            throw new InternalServerException("Internal error when fetching vehicle by VIN: " + e.getMessage());
-        }
+        return vehicleClient.getVehicleByVin(vin);
     }
 
     @Get(value = "/model/{model}", consumes = MediaType.APPLICATION_JSON)
     public List<VehicleDTO> getAllVehiclesByModel(@QueryValue String model) {
-        try {
-            return vehicleClient.getAllVehiclesByModel(model);
-        } catch (BadRequestException e) {
-            throw new BadRequestException("Bad request when fetching vehicles by model: " + e.getMessage());
-        } catch (InternalServerException e) {
-            throw new InternalServerException("Internal error when fetching vehicles by model: " + e.getMessage());
-        }
+        return vehicleClient.getAllVehiclesByModel(model);
     }
 
     @Get(value = "/getall", consumes = MediaType.APPLICATION_JSON)
     public List<VehicleDTO> getAllVehicles() {
-        try {
-            return vehicleClient.getAllVehicles();
-        } catch (BadRequestException e) {
-            throw new BadRequestException("Bad request when fetching all vehicles: " + e.getMessage());
-        } catch (InternalServerException e) {
-            throw new InternalServerException("Internal error when fetching all vehicles: " + e.getMessage());
-        }
+        return vehicleClient.getAllVehicles();
     }
 
     @Get("/view/{filename}")
     @Secured(SecurityRule.IS_ANONYMOUS)
     public HttpResponse<?> viewImage(@PathVariable String filename) {
-        try {
-            return vehicleClient.viewImage(filename);
-        } catch (BadRequestException e) {
-            throw new BadRequestException("Bad request when viewing image: " + e.getMessage());
-        } catch (InternalServerException e) {
-            throw new InternalServerException("Internal error when viewing image: " + e.getMessage());
-        }
+        return vehicleClient.viewImage(filename);
     }
 
-    @Error(status = HttpStatus.NOT_FOUND, global = true)
-    public HttpResponse<ErrorResponse> handleNotFound(HttpRequest<?> request) {
-        throw new NotFoundException("Endpoint " + request.getPath() + " not found");
-    }
 
-    @Error(status = HttpStatus.METHOD_NOT_ALLOWED, global = true)
-    public HttpResponse<ErrorResponse> handleMethodNotAllowed(HttpRequest<?> request) {
-        throw new MethodNotAllowedException("Method " + request.getMethod() + " not allowed for " + request.getPath());
-    }
 }
